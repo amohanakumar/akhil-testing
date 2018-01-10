223b88c4f9ab9ae34ddcc69dc4997cedfe2a5624
Akhil KM <akhil@kuberiter.com>
2018-01-10 13:16:26 +0530

    git change BV

--- add.c
+++ add.c
@@ -3,7 +3,8 @@
 void main() {
 	int firstNumber, secondNumber, sumOfTwoNumbers;
 	printf("Enter two integers z,f: ");
-	printf("Hello This is shabina")
+	printf("Hello This is shabina");
+	print("Hello This is Barabharan");
 	scanf("%d%d", &firstNumber, &secondNumber);
 	printf("Hello this is Akhil");
 	sumOfTwoNumbers = firstNumber + secondNumber;
