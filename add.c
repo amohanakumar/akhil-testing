2c048a74d2c4359156ded85d62400bd32708de98
Akhil KM <akhil@kuberiter.com>
2018-01-10 11:33:01 +0530

    first commit

--- add.c
+++ add.c
@@ -4,6 +4,7 @@ void main() {
 	int firstNumber, secondNumber, sumOfTwoNumbers;
 	printf("Enter two integers c,d: ");
 	scanf("%d%d", &firstNumber, &secondNumber);
+	printf("Hello this is Akhil");
 	sumOfTwoNumbers = firstNumber + secondNumber;
 	printf("%d + %d = %d", firstNumber,secondNumber,sumOfTwoNumbers);
 }
