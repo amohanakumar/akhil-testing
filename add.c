3a03b1eeacc3225043b01e35b2e1758a6db2026e
Akhil KM <akhil@kuberiter.com>
2018-01-10 09:16:30 +0530

    second commit

--- add.c
+++ add.c
@@ -2,7 +2,7 @@
 
 void main() {
 	int firstNumber, secondNumber, sumOfTwoNumbers;
-	printf("Enter two integers: ");
+	printf("Enter two integers a,b: ");
 	scanf("%d%d", &firstNumber, &secondNumber);
 	sumOfTwoNumbers = firstNumber + secondNumber;
 	printf("%d + %d = %d", firstNumber,secondNumber,sumOfTwoNumbers);
